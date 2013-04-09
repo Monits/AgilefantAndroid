fac776a7b79548ac0cc42c89d73818d4cc23bcd0
Ivan Corbalan <icorbalan@monits.com>
2013-04-09 16:58:09 -0300

    add slash

---
+++ src/com/monits/agilefant/activity/SplashActivity.java
@@ -0,0 +1,77 @@
+package com.monits.agilefant.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.inject.Inject;
+import com.monits.agilefant.R;
+import com.monits.agilefant.exception.RequestException;
+import com.monits.agilefant.model.Product;
+import com.monits.agilefant.service.BacklogService;
+import com.monits.agilefant.service.UserService;
+import com.monits.agilefant.task.LoginAsyncTask;
+
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends RoboActivity{
+
+	private int minDisplayMs = 2000;
+
+	@Inject
+	private UserService userService;
+
+	@Inject
+	private SharedPreferences sharedPreferences;
+
+	@Inject
+	private BacklogService backlogService;
+
+	@InjectView(R.id.progressbar)
+	private LinearLayout progressBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		final long start = System.currentTimeMillis();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				if (!userService.isLoggedIn()) {
+					final long duration = System.currentTimeMillis() - start;
+					if (duration < minDisplayMs) {
+						try {
+							Thread.sleep(minDisplayMs - duration);
+						} catch (InterruptedException e) {
+							Thread.interrupted();
+						}
+					}
+
+					startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+				} else {
+					progressBar.setVisibility(View.VISIBLE);
+					try {
+						userService.login(sharedPreferences.getString(UserService.DOMAIN_KEY, ""), sharedPreferences.getString(UserService.USER_NAME_KEY, ""), sharedPreferences.getString(UserService.PASSWORD_KEY, ""));
+
+						List<Product> allBacklogs = backlogService.getAllBacklogs();
+						Intent intent = new Intent(SplashActivity.this, AllBackLogsActivity.class);
+						intent.putExtra(LoginAsyncTask.ALL_BACKLOGS, new ArrayList<Product>(allBacklogs));
+						startActivity(intent);
+
+					} catch (RequestException e) {
+						startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+					}
+				}
+			}
+		}).start();
+	}
+}
