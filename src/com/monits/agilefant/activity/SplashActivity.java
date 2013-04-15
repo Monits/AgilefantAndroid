c3c8d51c41e99fdcc5dc4b2ea0d1fa457e2bf1c3
Gaston Nicolas Muñiz <gmuniz@monits.com>
2013-04-15 19:38:45 -0300

    Fixed issue when pausing app during the splash.

--- src/com/monits/agilefant/activity/SplashActivity.java
+++ src/com/monits/agilefant/activity/SplashActivity.java
@@ -5,20 +5,22 @@ import java.util.List;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
+import roboguice.util.RoboAsyncTask;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.inject.Inject;
 import com.monits.agilefant.R;
-import com.monits.agilefant.exception.RequestException;
 import com.monits.agilefant.model.Product;
 import com.monits.agilefant.service.BacklogService;
 import com.monits.agilefant.service.UserService;
 import com.monits.agilefant.task.LoginAsyncTask;
 
 @ContentView(R.layout.activity_splash)
-public class SplashActivity extends RoboActivity{
+public class SplashActivity extends RoboActivity {
 
 	@Inject
 	private UserService userService;
@@ -29,6 +31,8 @@ public class SplashActivity extends RoboActivity{
 	@Inject
 	private BacklogService backlogService;
 
+	private LoginTask loginTask;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,32 +40,81 @@ public class SplashActivity extends RoboActivity{
 		if (!userService.isLoggedIn()) {
 			startHomeActivity();
 		} else {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						boolean isLoggedIn = userService.login(sharedPreferences.getString(UserService.DOMAIN_KEY, ""), sharedPreferences.getString(UserService.USER_NAME_KEY, ""), sharedPreferences.getString(UserService.PASSWORD_KEY, ""));
-
-						if (isLoggedIn) {
-							List<Product> allBacklogs = backlogService.getAllBacklogs();
-							Intent intent = new Intent(SplashActivity.this, AllBackLogsActivity.class);
-							intent.putExtra(LoginAsyncTask.ALL_BACKLOGS, new ArrayList<Product>(allBacklogs));
-							startActivity(intent);
-						} else {
-							startHomeActivity();
-						}
-
-
-					} catch (RequestException e) {
-						startHomeActivity();
-					}
-				}
-			}).start();
+			loginTask = new LoginTask(this);
+			loginTask.execute();
 		}
+	}
 
+	@Override
+	protected void onPause() {
+		loginTask.cancel(true);
+		super.onPause();
 	}
 
 	private void startHomeActivity() {
 		startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+	}
+
+	class LoginTask extends RoboAsyncTask<List<Product>> {
+
+		private boolean successfullyLoggedIn;
+		private boolean isInterrupted;
+		private List<Product> backlogs;
+
+		protected LoginTask(Context context) {
+			super(context);
+		}
+
+		@Override
+		protected void onPreExecute() throws Exception {
+			isInterrupted = false;
+			super.onPreExecute();
+		}
+
+		@Override
+		public List<Product> call() throws Exception {
+			successfullyLoggedIn = userService.login(
+					sharedPreferences.getString(UserService.DOMAIN_KEY, ""),
+					sharedPreferences.getString(UserService.USER_NAME_KEY, ""),
+					sharedPreferences.getString(UserService.PASSWORD_KEY, ""));
+
+			if (successfullyLoggedIn) {
+				return backlogService.getAllBacklogs();
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onSuccess(List<Product> backlogs) throws Exception {
+			this.backlogs = backlogs;
+			super.onSuccess(backlogs);
+		}
+
+		@Override
+		protected void onException(Exception e) throws RuntimeException {
+			super.onException(e);
+			Log.e(this.getClass().getName(), "Failed to retrieve backlogs", e);
+		}
+
+		@Override
+		protected void onInterrupted(Exception e) {
+			isInterrupted = true;
+			super.onInterrupted(e);
+		}
+		@Override
+		protected void onFinally() throws RuntimeException {
+			if (!isInterrupted) {
+				if (successfullyLoggedIn) {
+					Intent intent = new Intent(SplashActivity.this, AllBackLogsActivity.class);
+					intent.putExtra(LoginAsyncTask.ALL_BACKLOGS, new ArrayList<Product>(backlogs));
+					startActivity(intent);
+				} else {
+					startHomeActivity();
+				}
+			}
+
+			super.onFinally();
+		}
 	}
 }
