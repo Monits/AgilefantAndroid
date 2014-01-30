d9840793471272c41402ce100a869f49e01be1f2
Gaston Nicolas Muñiz <gmuniz@monits.com>
2014-01-30 14:07:26 -0300

    Implemented change story state in leaf stories.
    Test Plan: Navigate to a project details screen, tap on a leaf story's state, change it.
    
    Reviewers: jmsotuyo
    
    Reviewed By: jmsotuyo
    
    CC: jmsotuyo
    
    Differential Revision: http://ph.monits.com/D2636

--- src/com/monits/agilefant/adapter/ProjectLeafStoriesAdapter.java
+++ src/com/monits/agilefant/adapter/ProjectLeafStoriesAdapter.java
@@ -1,56 +1,47 @@
 package com.monits.agilefant.adapter;
 
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 import roboguice.RoboGuice;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Response.ErrorListener;
-import com.android.volley.Response.Listener;
-import com.android.volley.VolleyError;
-import com.google.inject.Inject;
 import com.monits.agilefant.R;
-import com.monits.agilefant.activity.IterationActivity;
-import com.monits.agilefant.fragment.user_chooser.UserChooserFragment;
-import com.monits.agilefant.fragment.user_chooser.UserChooserFragment.OnUsersSubmittedListener;
+import com.monits.agilefant.listeners.AdapterViewActionListener;
 import com.monits.agilefant.model.Iteration;
 import com.monits.agilefant.model.Story;
-import com.monits.agilefant.model.User;
-import com.monits.agilefant.service.IterationService;
-import com.monits.agilefant.service.MetricsService;
 import com.monits.agilefant.util.IterationUtils;
 
-public class ProjectLeafStoriesAdapter extends BaseAdapter implements Observer {
-
-	@Inject
-	private IterationService iterationService;
-
-	@Inject
-	private MetricsService metricsService;
+public class ProjectLeafStoriesAdapter extends BaseAdapter {
 
 	private final Context context;
-	private final FragmentManager fragmentManager;
 
 	private List<Story> stories;
 
-	public ProjectLeafStoriesAdapter(final Context context, final FragmentManager fm) {
-		this.context = context;
-		this.fragmentManager = fm;
+	private AdapterViewActionListener<Story> actionListener;
+	private final OnClickListener onClickListener;
 
+	public ProjectLeafStoriesAdapter(final Context context) {
 		RoboGuice.injectMembers(context, this);
+
+		this.context = context;
+
+		onClickListener = new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				final Integer position = (Integer) v.getTag();
+
+				if (actionListener != null && position != null) {
+					actionListener.onAction(v, getItem(position));
+				}
+			}
+		};
 	}
 
 	@Override
@@ -96,93 +87,21 @@ public class ProjectLeafStoriesAdapter extends BaseAdapter implements Observer {
 
 		holder.responsibles.setText(
 				IterationUtils.getResposiblesDisplay(story.getResponsibles()));
-		holder.responsibles.setOnClickListener(new OnClickListener() {
+		holder.responsibles.setTag(position);
+		holder.responsibles.setOnClickListener(onClickListener);
 
-			@Override
-			public void onClick(final View v) {
-				story.addObserver(ProjectLeafStoriesAdapter.this);
-
-				final Fragment fragment = UserChooserFragment.newInstance(
-						story.getResponsibles(),
-						new OnUsersSubmittedListener() {
-
-							@Override
-							public void onSubmitUsers(final List<User> users) {
-								metricsService.changeStoryResponsibles(
-										users,
-										story,
-										new Listener<Story>() {
-
-											@Override
-											public void onResponse(final Story project) {
-												Toast.makeText(context, R.string.feedback_success_updated_project, Toast.LENGTH_SHORT).show();
-											}
-										},
-										new ErrorListener() {
-
-											@Override
-											public void onErrorResponse(final VolleyError arg0) {
-												Toast.makeText(context, R.string.feedback_failed_update_project, Toast.LENGTH_SHORT).show();
-											}
-										});
-							}
-						});
-
-				fragmentManager.beginTransaction()
-					.add(android.R.id.content, fragment)
-					.addToBackStack(null)
-					.commit();
-			}
-		});
-
-		holder.state.setTextColor(context.getResources().getColor(IterationUtils.getStateTextColor(story.getState())));
 		holder.state.setText(IterationUtils.getStateName(story.getState()));
+		holder.state.setTextColor(context.getResources().getColor(IterationUtils.getStateTextColor(story.getState())));
 		holder.state.setBackgroundResource(IterationUtils.getStateBackground(story.getState()));
+		holder.state.setTag(position);
+		holder.state.setOnClickListener(onClickListener);
 
 		if (story.getIteration() != null) {
 			final Iteration iteration = story.getIteration();
 
 			holder.iteration.setText(iteration.getName());
-			holder.iteration.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(final View v) {
-
-					final ProgressDialog progressDialog = new ProgressDialog(context);
-					progressDialog.setIndeterminate(true);
-					progressDialog.setCancelable(false);
-					progressDialog.setMessage(context.getString(R.string.loading));
-					progressDialog.show();
-					iterationService.getIteration(
-							iteration.getId(),
-							new Listener<Iteration>() {
-
-								@Override
-								public void onResponse(final Iteration response) {
-									if (progressDialog != null && progressDialog.isShowing()) {
-										progressDialog.dismiss();
-									}
-
-									final Intent intent = new Intent(context, IterationActivity.class);
-									response.setParent(iteration.getParent());
-									intent.putExtra(IterationActivity.ITERATION, response);
-
-									context.startActivity(intent);
-								}
-							},
-							new ErrorListener() {
-
-								@Override
-								public void onErrorResponse(final VolleyError arg0) {
-									if (progressDialog != null && progressDialog.isShowing()) {
-										progressDialog.dismiss();
-									}
-
-									Toast.makeText(context, R.string.feedback_failed_retrieve_iteration, Toast.LENGTH_SHORT).show();
-								}
-							});
-				}
-			});
+			holder.iteration.setTag(position);
+			holder.iteration.setOnClickListener(onClickListener);
 		} else {
 			holder.iteration.setText(" - ");
 			holder.iteration.setOnClickListener(null);
@@ -197,16 +116,19 @@ public class ProjectLeafStoriesAdapter extends BaseAdapter implements Observer {
 		notifyDataSetChanged();
 	}
 
+	/**
+	 * Add a listener to intercept click events within row views separately.
+	 *
+	 * @param listener the listener to be set.
+	 */
+	public void setOnActionListener(final AdapterViewActionListener<Story> listener) {
+		this.actionListener = listener;
+	}
+
 	private static class ViewHolder {
 		TextView name;
 		TextView iteration;
 		TextView responsibles;
 		TextView state;
-	}
-
-	@Override
-	public void update(final Observable observable, final Object data) {
-		notifyDataSetChanged();
-		observable.deleteObserver(this);
 	}
 }
--- src/com/monits/agilefant/fragment/project/ProjectLeafStoriesFragment.java
+++ src/com/monits/agilefant/fragment/project/ProjectLeafStoriesFragment.java
@@ -1,9 +1,19 @@
 package com.monits.agilefant.fragment.project;
 
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 import roboguice.fragment.RoboFragment;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +26,21 @@ import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.google.inject.Inject;
 import com.monits.agilefant.R;
+import com.monits.agilefant.activity.IterationActivity;
 import com.monits.agilefant.adapter.ProjectLeafStoriesAdapter;
+import com.monits.agilefant.fragment.user_chooser.UserChooserFragment;
+import com.monits.agilefant.fragment.user_chooser.UserChooserFragment.OnUsersSubmittedListener;
+import com.monits.agilefant.listeners.AdapterViewActionListener;
 import com.monits.agilefant.model.Backlog;
+import com.monits.agilefant.model.Iteration;
+import com.monits.agilefant.model.StateKey;
 import com.monits.agilefant.model.Story;
+import com.monits.agilefant.model.User;
+import com.monits.agilefant.service.IterationService;
+import com.monits.agilefant.service.MetricsService;
 import com.monits.agilefant.service.ProjectService;
 
-public class ProjectLeafStoriesFragment extends RoboFragment {
+public class ProjectLeafStoriesFragment extends RoboFragment implements Observer {
 
 	private static final String BACKLOG = "PROJECT_BACKLOG";
 
@@ -29,6 +48,12 @@ public class ProjectLeafStoriesFragment extends RoboFragment {
 
 	@Inject
 	private ProjectService projectService;
+
+	@Inject
+	private MetricsService metricsService;
+
+	@Inject
+	private IterationService iterationService;
 
 	private ViewFlipper viewFlipper;
 	private ListView storiesListView;
@@ -64,10 +89,115 @@ public class ProjectLeafStoriesFragment extends RoboFragment {
 			.inflate(R.layout.fragment_project_leaf_stories, container, false);
 
 		viewFlipper = (ViewFlipper) view.findViewById(R.id.root_flipper);
+
+		final FragmentActivity context = getActivity();
+		storiesAdapter = new ProjectLeafStoriesAdapter(context);
+		storiesAdapter.setOnActionListener(new AdapterViewActionListener<Story>() {
+
+			@Override
+			public void onAction(final View view, final Story object) {
+
+				object.addObserver(ProjectLeafStoriesFragment.this);
+
+				switch (view.getId()) {
+					case R.id.column_responsibles:
+						final Fragment fragment = UserChooserFragment.newInstance(
+								object.getResponsibles(),
+								new OnUsersSubmittedListener() {
+
+									@Override
+									public void onSubmitUsers(final List<User> users) {
+										metricsService.changeStoryResponsibles(
+												users,
+												object,
+												new Listener<Story>() {
+
+													@Override
+													public void onResponse(final Story project) {
+														Toast.makeText(context, R.string.feedback_success_updated_project, Toast.LENGTH_SHORT).show();
+													}
+												},
+												new ErrorListener() {
+
+													@Override
+													public void onErrorResponse(final VolleyError arg0) {
+														Toast.makeText(context, R.string.feedback_failed_update_project, Toast.LENGTH_SHORT).show();
+													}
+												});
+									}
+								});
+
+						context.getSupportFragmentManager().beginTransaction()
+							.add(android.R.id.content, fragment)
+							.addToBackStack(null)
+							.commit();
+
+						break;
+					case R.id.column_context:
+						final Iteration iteration = object.getIteration();
+
+						final ProgressDialog progressDialog = new ProgressDialog(context);
+						progressDialog.setIndeterminate(true);
+						progressDialog.setCancelable(false);
+						progressDialog.setMessage(context.getString(R.string.loading));
+						progressDialog.show();
+						iterationService.getIteration(
+								iteration.getId(),
+								new Listener<Iteration>() {
+
+									@Override
+									public void onResponse(final Iteration response) {
+										if (progressDialog != null && progressDialog.isShowing()) {
+											progressDialog.dismiss();
+										}
+
+										final Intent intent = new Intent(context, IterationActivity.class);
+										response.setParent(iteration.getParent());
+										intent.putExtra(IterationActivity.ITERATION, response);
+
+										context.startActivity(intent);
+									}
+								},
+								new ErrorListener() {
+
+									@Override
+									public void onErrorResponse(final VolleyError arg0) {
+										if (progressDialog != null && progressDialog.isShowing()) {
+											progressDialog.dismiss();
+										}
+
+										Toast.makeText(context, R.string.feedback_failed_retrieve_iteration, Toast.LENGTH_SHORT).show();
+									}
+								});
+						break;
+
+					case R.id.column_state:
+						final OnClickListener onStoryStateSelectedListener = new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(final DialogInterface dialog, final int which) {
+								final StateKey state = StateKey.values()[which];
+
+								// In this case, setting story to done, doesn't prompt if you want to set all tasks to done
+								executeUpdateStoryTask(state, object, false);
+
+								dialog.dismiss();
+							}
+						};
+
+						final AlertDialog.Builder builder = new Builder(context);
+						builder.setTitle(R.string.dialog_state_title);
+						builder.setSingleChoiceItems(
+								StateKey.getDisplayStates(), object.getState().ordinal(), onStoryStateSelectedListener);
+						builder.show();
+
+						break;
+				}
+			}
+		});
+
 		storiesListView = (ListView) view.findViewById(R.id.stories_list);
 		storiesEmptyView = view.findViewById(R.id.stories_empty_view);
-
-		storiesAdapter = new ProjectLeafStoriesAdapter(getActivity(), getFragmentManager());
 		storiesListView.setEmptyView(storiesEmptyView);
 		storiesListView.setAdapter(storiesAdapter);
 
@@ -96,5 +226,44 @@ public class ProjectLeafStoriesFragment extends RoboFragment {
 				});
 
 		super.onViewCreated(view, savedInstanceState);
+	}
+
+	@Override
+	public void update(final Observable observable, final Object data) {
+		if (isVisible()) {
+			storiesAdapter.notifyDataSetChanged();
+			observable.deleteObserver(this);
+		}
+	}
+
+	/**
+	 * Configures and executes the {@link UpdateStoryTask}.
+	 *
+	 * @param state
+	 * @param story
+	 * @param allTasksToDone
+	 */
+	private void executeUpdateStoryTask(final StateKey state, final Story story, final boolean allTasksToDone) {
+		final FragmentActivity context = getActivity();
+		metricsService.changeStoryState(
+				state,
+				story,
+				allTasksToDone,
+				new Listener<Story>() {
+
+					@Override
+					public void onResponse(final Story arg0) {
+						Toast.makeText(
+								context, R.string.feedback_successfully_updated_story, Toast.LENGTH_SHORT).show();
+					}
+				},
+				new ErrorListener() {
+
+					@Override
+					public void onErrorResponse(final VolleyError arg0) {
+						Toast.makeText(
+								context, R.string.feedback_failed_update_story, Toast.LENGTH_SHORT).show();
+					}
+				});
 	}
 }
