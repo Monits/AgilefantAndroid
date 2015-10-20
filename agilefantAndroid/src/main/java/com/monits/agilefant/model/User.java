57907536fdc9059c957afebcb6e152b99c601ea8
Lgnanni <lgnanni@monits.com>
2015-10-20 15:07:41 -0300

    Fix user lint
    Summary: Added all the variables on the toString function
    
    Test Plan: Run linter and the warning is gone
    
    Reviewers: jmsotuyo
    
    Reviewed By: jmsotuyo
    
    Differential Revision: http://ph.monits.com/D13002

--- agilefantAndroid/src/main/java/com/monits/agilefant/model/User.java
+++ agilefantAndroid/src/main/java/com/monits/agilefant/model/User.java
@@ -1,16 +1,23 @@
 package com.monits.agilefant.model;
 
+
 import java.io.Serializable;
 
 import com.google.gson.annotations.SerializedName;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 public class User implements Serializable {
 
 	private static final long serialVersionUID = -642609196033074436L;
 
+	@SuppressFBWarnings(value = "MISSING_FIELD_IN_TO_STRING",
+			justification = "We need to ignore this field due to native method calling")
 	@SerializedName("id")
 	private long id;
 
+	@SuppressFBWarnings(value = "MISSING_FIELD_IN_TO_STRING",
+			justification = "We need to ignore this field due to native method calling")
 	@SerializedName("initials")
 	private String initials;
 
@@ -67,9 +74,11 @@ public class User implements Serializable {
 		this.fullName = fullName;
 	}
 
+
 	@Override
 	public String toString() {
 		// This value is shown when in landscape and using autocomplete.
+
 		return fullName;
 	}
 }
